e571260b7948b78a7834d35d48f115aefc5107fb
“Salma” <h.n222@yahoo.com>
2021-09-04 09:48:05 -0400

    Permutation.java implemented

--- src/string/problems/DeleteClass.java
+++ src/string/problems/DeleteClass.java
@@ -6,19 +6,14 @@ public class DeleteClass {
 
     public static void main(String args[])
     {
-        String original, reverse = ""; // Objects of String class
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter a string to check if it's a palindrome");
-        original = in.nextLine();
 
-        int length = original.length();
 
-        for (int i = length - 1; i >= 0; i--)
-            reverse = reverse + original.charAt(i);
+        for (String s: Permutation.getPermutation("abcd")){
+            System.out.println(s);
+        }
 
-        if (original.equals(reverse))
-            System.out.println("The string is a palindrome.");
-        else
-            System.out.println("The string isn't a palindrome.");
+
     }
+
+
 }
--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -1,5 +1,8 @@
 package string.problems;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Permutation {
 
     public static void main(String[] args) {
@@ -9,5 +12,48 @@ public class Permutation {
 
         Write Java program to compute all permutations of any given String
          */
+
+
+
+        for (String s: getPermutation("abcd")){
+            System.out.println(s);
+        }
+
     }
+
+
+    public static Set<String> getPermutation(String str) {
+
+        // create a set to avoid duplicate permutation
+        Set<String> permutations = new HashSet<String>();
+
+        // check if string is null
+        if (str == null) {
+            return null;
+        } else if (str.length() == 0) {
+            // terminating condition for recursion
+            permutations.add("");
+            return permutations;
+        }
+
+        // get the first character
+        char first = str.charAt(0);
+
+        // get the remaining substring
+        String sub = str.substring(1);
+
+        // make recursive call to getPermutation()
+        Set<String> words = getPermutation(sub);
+
+        // access each element from words
+        for (String strNew : words) {
+            for (int i = 0;i<=strNew.length();i++){
+
+                // insert the permutation to the set
+                permutations.add(strNew.substring(0, i) + first + strNew.substring(i));
+            }
+        }
+        return permutations;
+    }
+
 }
