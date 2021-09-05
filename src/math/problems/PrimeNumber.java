46e546a1f3c1b20beaf5f4bd42827cb896278fa1
“Salma” <h.n222@yahoo.com>
2021-09-05 19:00:57 -0400

    PrimeNumber class implemented

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -11,6 +11,34 @@ public class PrimeNumber {
          Print out the prime numbers in the given range.
          */
 
+
+        printPrimeNumbers(2, 100);
     }
+
+
+
+    public static void printPrimeNumbers(int firstNumber, int lastNumber) {
+
+        for (int i = firstNumber; i < lastNumber; i++) {
+
+            if(isPrime(i)){
+                System.out.print(i + ",");
+            }
+
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
